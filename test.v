f1e750190d02f2cc17479727d6b78ea7ce324121
nicolas.tabareau <nicolas.tabareau@inria.fr>
2017-10-23 11:35:43 +0200

    more tests

--- test.v
+++ test.v
@@ -19,10 +19,6 @@ Fixpoint IsVect A (n : nat) (l : list A) : Prop :=
   | S _ , nil => False
   | 0, cons _ _ => False
   | S n , cons _ l => IsVect _ n l end.
- 
-(* Inductive IsVect A : nat -> list A -> Prop := *)
-(* | nil0 : IsVect A 0 nil *)
-(* | cons0 : forall x n l, IsVect A n l -> IsVect A (S n) (cons x l). *)
                  
 Definition llist A n := {l : list A & IsVect A n l}. 
 
@@ -31,9 +27,6 @@ Definition cons_l : forall A , A -> forall n : nat, llist A n -> llist A (S n) :
 
 Definition nil_l : forall A , llist A 0 :=
   fun A => (nil ; I).
-
-(* Definition cons_l : forall A , A -> forall n : nat, llist A n -> llist A (S n) := *)
-(*   fun A x n l => (cons x (l.1) ; cons0 A x n l.1 l.2). *)
 
 Definition t_rect 
      : forall (A : Type) (P : forall n : nat, t A n -> Type),
@@ -62,24 +55,6 @@ fun (A : Type) (P : forall n : nat, llist A n -> Type) (f : P 0 (nil_l A))
   | cons _ _ , 0 => fun i => match i with end
   end) n X.1 X.2.
 
-Definition caseS' : forall {A : Type} (P : forall n : nat, t A (S n) -> Type),
-    (forall (h : A) (n : nat) (t : t A n), P n (Vector.cons A h n t)) ->
-    forall {n : nat} (v : t A (S n)), P n v :=
-  fun (A : Type) (P : forall n : nat, t A (S n) -> Type)
-       (H : forall (h : A) (n : nat) (t : t A n), P n (Vector.cons A h n t))
-       (n : nat) (v : t A (S n)) =>
-    t_rect A (fun n => match n with 0 => fun _ => unit | S m => P m end) tt
-                  (fun (h : A) (m : nat) (t0 : t A m) _ => H h m t0) (S n) v.
-
-Definition caseS'_l : forall {A : Type} (P : forall n : nat, llist A (S n) -> Type),
-       (forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t)) ->
-       forall {n : nat} (v : llist A (S n)), P n v  :=
-  fun (A : Type) (P : forall n : nat, llist A (S n) -> Type)
-       (H : forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t))
-       (n : nat) (v : llist A (S n)) =>
-    llist_rect A (fun n => match n with 0 => fun _ => unit | S m => P m end) tt
-                  (fun (h : A) (m : nat) (t0 : llist A m) _ => H h m t0) (S n) v.
-
 Extraction llist_rect.
 
 Definition caseS : forall {A : Type} (P : forall n : nat, t A (S n) -> Type),
@@ -88,13 +63,13 @@ Definition caseS : forall {A : Type} (P : forall n : nat, t A (S n) -> Type),
   fun (A : Type) (P : forall n : nat, t A (S n) -> Type)
       (H : forall (h : A) (n : nat) (t : t A n), P n (Vector.cons A h n t))
       (n : nat) (v : t A (S n)) =>
-    match
-      v as v0 in (t _ n0)
-      return
-      (match n0 as x return (t A x -> Type) with
+    let Q n0 v0 := match n0 as x return (t A x -> Type) with
        | 0 => fun _ : t A 0 => False -> IDProp
        | S n1 => fun v1 : t A (S n1) => P n1 v1
-       end v0)
+       end v0 in
+    match
+      v as v0 in (t _ n0) return
+      Q n0 v0
     with
     | Vector.nil _ => fun devil : False => False_ind IDProp devil
     | Vector.cons _ h n0 t0 => H h n0 t0
@@ -114,71 +89,11 @@ Definition caseS_l : forall {A : Type} (P : forall n : nat, llist A (S n) -> Typ
        | S n1 => fun v1 : llist A (S n1) => P n1 v1
        end (v0;H0))
     with
-    | nil, 0 => fun i (devil : False) => False_ind IDProp devil
+    | nil, 0 => fun i => match i with I => fun (devil : False) => False_ind IDProp devil end
     | nil, S _ => fun i => match i with end
     | cons h t0, S n0 => fun i => H h n0 (t0;i)
     | cons h t0, 0 => fun i => match i with end
     end v.2.
-
-
-Definition caseS_l :=
-(fun (A : Type) (P : forall n : nat, llist A (S n) -> Type)
-  (H : forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t))
-  (n : nat) (v : llist A (S n)) =>
-match
-  v.1 as v0, n as n0 
-  (* in (IsVect _ n0 l) *)
-  return
-    (match n0 as x return (llist A x -> Type) with
-     | 0 => fun _ : llist A 0 => False -> IDProp
-     | S n1 => fun v1 : llist A (S n1) => P n1 v1
-     end (v0;v.2))
-with
-| nil, _ => fun devil : False => False_ind IDProp devil
-| cons h t0, S n => H h n (t0;v.2)
-end)
-     : forall {A : Prop} (P : forall n : nat, llist A (S n) -> Prop),
-       (forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t)) ->
-       forall {n : nat} (v : llist A (S n)), P n v.
-
-
-
-
-
-Definition caseS_l := 
-(fun (A : Type) (P : forall n : nat, llist A (S n) -> Prop)
-  (H : forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t)) 
-  (n : nat) (v : llist A (S n)) =>
-match
-  v.2 as v20 in (IsVect _ n0 l)
-  return
-    (match n0 as x return (llist A x -> Prop) with
-     | 0 => fun _ : llist A 0 => False -> IDProp
-     | S n1 => fun v1 : llist A (S n1) => P n1 v1
-     end (l;v20))
-with
-| nil0 _ => fun devil : False => False_ind IDProp devil
-| cons0 _ h n t0 t1 => H h n (t0;t1)
-end)
-     : forall {A : Prop} (P : forall n : nat, llist A (S n) -> Prop),
-       (forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t)) ->
-       forall {n : nat} (v : llist A (S n)), P n v.
-
-Definition caseS_l := 
-(fun (A : Type) (P : forall n : nat, llist A (S n) -> Prop)
-  (H : forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t)) 
-  (n : nat) (v : llist A (S n)) =>
-match v.2  
-with
-| nil0 _ => fun devil : False => False_ind IDProp devil
-| cons0 _ h n t0 t1 => H h n (t0;t1)
-end)
-     : forall {A : Prop} (P : forall n : nat, llist A (S n) -> Prop),
-       (forall (h : A) (n : nat) (t : llist A n), P n (cons_l A h n t)) ->
-       forall {n : nat} (v : llist A (S n)), P n v.
-
-
-
 
 Definition nth {A : Type} : forall (m : nat), t A m -> Fin.t m -> A :=
 fix nth_fix (m : nat) (v' : t A m) (p : Fin.t m) {struct v'} : A :=
@@ -190,15 +105,79 @@ fix nth_fix (m : nat) (v' : t A m) (p : Fin.t m) {struct v'} : A :=
         (fun (_ : A) (n0 : nat) (t : t A n0) (p0 : Fin.t n0) => nth_fix n0 t p0) v p'
   end v'.
 
-Definition nth_l {A:Prop} : forall (m : nat), llist A m -> Fin.t m -> A := fun n (v:llist A n) =>
-(fix nth_fix (m:nat) l (v' : IsVect A m l) (p : Fin.t m) {struct v'} : A :=
-match p in Fin.t m' return forall x,  IsVect A m' x -> A with
- |Fin.F1 => fun X Y => caseS_l (fun n v' => A) (fun h n t => h) (X;Y)
- |Fin.FS p' => fun v1 v2 => (caseS_l (fun n v' => Fin.t n -> A)
-   (fun h n t p0 => nth_fix n t.1 t.2 p0) (v1;v2)) p'
-end l v') n v.1 v.2.
+Definition nth_l {A:Type} : forall (m : nat), llist A m -> Fin.t m -> A :=
+  fun m X => 
+(fix nth_fix (m : nat) (v' : list A) (H : IsVect A m v') (p : Fin.t m) {struct v'} : A :=
+  match p in (Fin.t m') return (forall (v' : list A) (H : IsVect A m' v'), A) with
+  | @Fin.F1 n => fun X Y => caseS_l (fun (n0 : nat) (_ : llist A (S n0)) => A) (fun (h : A) (n0 : nat) (_ : llist A n0) => h) (X;Y)
+  | @Fin.FS n p' =>
+      fun X Y => 
+      caseS_l (fun (n0 : nat) (_ : llist A (S n0)) => Fin.t n0 -> A)
+        (fun (_ : A) (n0 : nat) (t : llist A n0) (p0 : Fin.t n0) => nth_fix n0 t.1 t.2 p0) (X;Y) p'
+  end v' H) m X.1 X.2.
+
 
 Extraction Language Ocaml.
 
-Extraction llist. 
+Extraction caseS_l. 
 
+Extraction nth_l. 
+
+Definition caseS' {A} {n : nat} (v : t A (S n)) : forall (P : t A (S n) -> Type)
+  (H : forall h t, P (Vector.cons _ h _ t)), P v :=
+  match v with
+  | Vector.cons _ h _ t => fun P H => H h t
+  | _ => fun devil => False_rect (@IDProp) devil
+  end.
+
+Definition caseS'_l {A} {n : nat} (v : llist A (S n)) : forall (P : llist A (S n) -> Type)
+                                                               (H : forall h t, P (cons_l _ h _ t)), P v :=
+  let Q n0 v0 := (match n0 as x return (llist A x -> Type) with
+     | 0 => fun _ : llist A 0 => False -> IDProp
+     | S n1 =>
+         fun v1 : llist A (S n1) => forall P : llist A (S n1) -> Type, (forall (h : A) (t : llist A n1), P (cons_l A h n1 t)) -> P v1
+   end v0) in 
+  match v.1 as l0 , S n as n0
+  return forall H0 : IsVect A n0 l0, Q n0 (l0;H0)
+  with
+  | h :: t, S m => fun i P H => H h (t;i)
+  | h :: t, 0 => fun i => match i with end                                     
+  | nil , 0 => fun i devil => False_rect (@IDProp) devil
+  | nil , S _ => fun i => match i with end                                     
+  end v.2.
+
+Fixpoint replace {A n} (v : t A n) (p: Fin.t n) (a : A) {struct p}: t A n :=
+  match p with
+  | @Fin.F1 k => fun v': t A (S k) => caseS' v' _ (fun h t => Vector.cons _ a _ t)
+  | @Fin.FS k p' => fun v' : t A (S k) =>
+    (caseS' v' (fun _ => t A (S k)) (fun h t => Vector.cons _ h _ (replace t p' a)))
+  end v.
+
+Fixpoint replace_l {A n} (v : llist A n) (p: Fin.t n) (a : A) {struct p}: llist A n :=
+  match p with
+  | @Fin.F1 k => fun (l : list A) (H : IsVect A (S k) l) => caseS'_l (l;H) _ (fun h t => cons_l _ a _ t)
+  | @Fin.FS k p' => fun (l : list A) (H : IsVect A (S k) l) => 
+    (caseS'_l (l;H) (fun _ => llist A (S k)) (fun h t => cons_l _ h _ (replace_l t p' a)))
+  end v.1 v.2.
+
+Require Import Arith_base.
+
+Fixpoint take {A} {n} (p:nat) (le:p <= n) (v:t A n) : t A p := 
+  match p as p return p <= n -> t A p with 
+  | 0 => fun _ => Vector.nil _ 
+  | S p' => match v in t _ n return S p' <= n -> t A (S p') with
+    | Vector.nil _=> fun le => False_rect _ (Nat.nle_succ_0 p' le)
+    | Vector.cons _ x _ xs => fun le => Vector.cons _ x _ (take p' (le_S_n p' _ le) xs)
+    end 
+  end le.
+
+Fixpoint take_l {A} {n} (p:nat) (le:p <= n) (v:llist A n) : llist A p := 
+  match p as p return p <= n -> llist A p with 
+  | 0 => fun _ => nil_l _ 
+  | S p' => match v.1 as l0, n as n0 return forall H0 : IsVect A n0 l0, S p' <= n0 -> llist A (S p') with
+    | nil, 0 => fun i le => False_rect _ (Nat.nle_succ_0 p' le)
+    | nil, S _ => fun i le => match i with end
+    | cons x xs, S _ => fun i le => cons_l _ x _ (take_l p' (le_S_n p' _ le) (xs;i))
+    | cons _ _ , 0 => fun i le => match i with end
+    end v.2
+  end le.
