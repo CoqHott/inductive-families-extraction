387448adc1b2b369897d545791d5ae41ad966f45
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2018-01-16 18:42:45 +0100

    Update script

--- stats.sh
+++ stats.sh
@@ -1,11 +1,17 @@
-NB_LEMMAS=`grep '^Lemma' good_bad_ugly.v | wc -l`
-NB_ADMITTED=`grep 'Admitted.' good_bad_ugly.v | wc -l`
-NB_FAILED_LEMMAS=`grep '^Fail Lemma' good_bad_ugly.v | wc -l`
-NB_DEF=`grep '^\(Definition\|Fixpoint\) ' good_bad_ugly.v | wc -l`
-NB_FAILED_DEF=`grep '^Fail ' good_bad_ugly.v | grep -v '^Fail Lemma' | wc -l`
+SRC=good_bad_ugly.v
+
+NB_LEMMAS=`grep '^Lemma' $SRC | wc -l`
+NB_ADMITTED=`grep 'Admitted.' $SRC | wc -l`
+NB_FAILED_LEMMAS=`grep '^Fail Lemma' $SRC | wc -l`
+NB_DEF=`grep '^\(Definition\|Fixpoint\) ' $SRC | wc -l`
+NB_FAILED_DEF=`grep '^Fail ' $SRC | grep -v '^Fail Lemma' | wc -l`
 
 echo "Number of functions: $NB_DEF"
 echo "Number of painful functions: $NB_FAILED_DEF"
 echo "Number of lemmas: $NB_LEMMAS"
 echo "Number of painful lemmas: $NB_ADMITTED"
 echo "Number of unstated lemmas: $NB_FAILED_LEMMAS"
+
+echo ""
+echo "Summary of failures: "
+grep 'FAILED(' $SRC
